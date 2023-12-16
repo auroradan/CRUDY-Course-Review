ffddedf654c6686637f9804abb496253c03d1f18
Dan Tran <103843222+auroradan@users.noreply.github.com>
2023-12-16 17:33:06 -0500

    Delete hw6-hw6-nay3at-kus4ws-bdz4ze-xvs5zs directory

--- hw6-hw6-nay3at-kus4ws-bdz4ze-xvs5zs/src/main/java/edu/virginia/sde/reviews/CourseUpdateCallback.java
+++
@@ -1,7 +0,0 @@
-package edu.virginia.sde.reviews;
-
-//AI Agent: ChatGPT
-//Prompt: how to make it so after database.commit() the loadCourses() function is called from CourseSearchController
-public interface CourseUpdateCallback {
-    void onCourseUpdated();
-}
